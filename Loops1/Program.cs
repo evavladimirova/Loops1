ab0152175d8e82ef85b02dfc00f69ac1280658a4
sport <sport@LAPTOP-61I2ACB0>
2023-09-15 20:36:18 +0300

    Add project files.

---
+++ Loops1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int row, i, j;
+
+Console.Write("Enter the number of rows: ");
+row = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("");
+
+            
+    for (i = 1; i <= row; i++)
+        {
+             
+          for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+             
+                Console.WriteLine("");
+         
+        }
